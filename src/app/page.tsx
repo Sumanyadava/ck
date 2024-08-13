d907dd99a3c8a9dd793d3e4b15fb8012e6fae19e
Sumanyadava <ansum2411@gmail.com>
2024-08-13 15:41:27 +0530

    fix page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
               </div>
             </div>
 
-            <DockDemo />
+            {/* <DockDemo /> */}
             <Footer />
             {/* //commits nav scratch */}
           </motion.div>
